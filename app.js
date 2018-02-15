2a11467f71618963d2a318f6bcf60770fd1f0507
Edgar Nightingale <edgar.nightingale@btinternet.com>
2018-02-15 21:33:52 +0000

    fix: Required inputs now function correctly

--- app.js
+++ app.js
@@ -1,10 +1,6 @@
 // Variable for contact form
 var contactForm = $('#contact');
-
-// Polyfill for unsupported form attribute in Internet Explorer
-$('button[type=submit]').click(function() {
-    contactForm.submit();
-});
+var responseContainer = $('#form-inputs');
 
 // Add submit event listener to the form
 contactForm.submit(function(e) {
@@ -27,13 +23,48 @@ contactForm.submit(function(e) {
 
 	// Display thank you message if request sent successfully
     request.done(function (response, textStatus, jqXHR){
-        contactForm.html(response);
+        responseContainer.html(response);
     });
 
     // Inform user if message fails to send
     request.fail(function (jqXHR, textStatus, errorThrown){
-       	contactForm.html('<p>Sorry there was an error sending your message, please e-mail me directly at <a href="mailto:edgar.nightingale@btinternet.com" class="email">edgar.nightingale@btinternet.com</a> instead.</p>');
+       	responseContainer.html('<p>Sorry there was an error sending your message, please e-mail me directly at <a href="mailto:edgar.nightingale@btinternet.com" class="email">edgar.nightingale@btinternet.com</a> instead.</p>');
         // Log the error to the console
         console.error("The following error occurred: " + textStatus, errorThrown);
     });
+});
+
+// Function for smooth scrolling on all same page links
+// Select all links with hashes and remove links that don't actually link to anything
+$('a[href*="#"]').not('[href="#"]').not('[href="#0"]').click(function(event) {
+	// On-page links
+	if (location.pathname.replace(/^\//, '') == this.pathname.replace(/^\//, '') && location.hostname == this.hostname) {
+
+		// Figure out element to scroll to
+		var target = $(this.hash);
+		target = target.length ? target : $('[name=' + this.hash.slice(1) + ']');
+
+		// Does a scroll target exist?
+		if (target.length) {
+			// Only prevent default if animation is actually gonna happen
+			event.preventDefault();
+
+			// Initiate scroll animation
+			$('html, body').animate({scrollTop: target.offset().top}, 1000, function() {
+				// Callback after animation
+				// Must change focus!
+				var $target = $(target);
+				$target.focus();
+
+				// Check if the target was focused
+				if ($target.is(":focus")) {
+			    	return false;
+				} else {
+				// Add tabindex for elements not focusable and focus again
+				$target.attr('tabindex','-1');
+				$target.focus();
+			  	};
+			});
+		}
+	}
 });
