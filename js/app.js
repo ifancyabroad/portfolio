55818dc57d6b68bf0102d68ac77785159719d2c3
ifancyabroad <edgar.nightingale88@gmail.com>
2019-02-02 14:32:56 +0000

    Addded arena section

--- js/app.js
+++ js/app.js
@@ -14,12 +14,12 @@ $(document).ready(function(){
       600:{
         items:2
       },
-      800:{
-      	items:2
-      },
       1000:{
         items:3
-      }
+			},
+			1400:{
+				items:4
+			}
     }
 	})
 
